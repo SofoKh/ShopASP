7f5a6edd5241578e472d3f82ef9610cf35963dd4
sopok <sopok@DESKTOP-9ABPMKH>
2023-02-01 20:59:42 +0400

    Add project files.

---
+++ Shop/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserDBContext db;
+
+        public UserController(UserDBContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<ActionResult<IEnumerable<User>>> GetAll()
+        {
+            return await this.db.Users.ToListAsync();
+        }
+
+        [HttpGet("[action]/{Id}")]
+
+        public async Task<ActionResult<User>> GetById(int id)
+        {
+            var user = await this.db.Users.FirstOrDefaultAsync(x => x.ID == id);
+
+            if (user == null)
+                return NotFound();
+
+            return new ObjectResult(user);
+        }
+    }
+}
---
+++ Shop/UserDBcontext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Shop
+{
+    public partial class UserDBContext : DbContext
+    {
+        public UserDBContext()
+        {
+        }
+
+        public UserDBContext(DbContextOptions<UserDBContext> options) : base(options)
+        {
+        }
+        
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(local);Database=ShopAPI;Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer("Server=(local);Database=ShopAPI;Trusted_Connection=True;", options => options.EnableRetryOnFailure().UseSslStream(ssl => ssl.AcceptAnyServerCertificate(true)));
+        }*/
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+                var connectionString = configuration.GetConnectionString("Server = (local); Database = ShopAPI; Trusted_Connection = True; ");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+    }
+}
